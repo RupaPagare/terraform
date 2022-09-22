a937824709bc5ef0250125643c05938febd3af33
RupaPagare <104555021+RupaPagare@users.noreply.github.com>
2022-09-22 15:24:20 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ec2-terraform" {
               ami ="ami-06489866022e12a14"
               instance_type = "t2.micro"
               security_groups = [aws_security_group.TerraformEc2_security1.name]
-              key_name = "ec2instance"
+              key_name = "Rupa.pem"
               tags = { ec2_create = "instance1"}
 }
 resource "aws_default_vpc" "main" {
@@ -17,5 +17,5 @@ resource "aws_default_vpc" "main" {
 resource "aws_ebs_volume" "vol" {
               availibility_zone = "ap-south-1"
               size              = 8
-              tags = { key_name = "ec2instance"}
+              tags = { key_name = "Rupa.pem"}
 }
